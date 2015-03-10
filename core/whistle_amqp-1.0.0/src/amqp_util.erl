b3986441659ef38f1e9106f0831863d4ecb70730
Pierre Fenoll <pierrefenoll@gmail.com>
2015-03-10 14:37:38 -0700

    revert

--- core/whistle_amqp-1.0.0/src/amqp_util.erl
+++ core/whistle_amqp-1.0.0/src/amqp_util.erl
@@ -169,9 +169,7 @@ presence_publish(Routing, Payload) ->
     presence_publish(Routing, Payload, ?DEFAULT_CONTENT_TYPE).
 presence_publish(Routing, Payload, ContentType) ->
     presence_publish(Routing, Payload, ContentType, []).
-presence_publish(Routing, Payload, ContentType, Opts0) ->
-    Opts = [{'immediate', 'true'}
-           ] ++ Opts0,
+presence_publish(Routing, Payload, ContentType, Opts) ->
     basic_publish(?EXCHANGE_PRESENCE, Routing, Payload, ContentType, Opts).
 
 -spec notifications_publish(ne_binary(), amqp_payload()) -> 'ok'.
